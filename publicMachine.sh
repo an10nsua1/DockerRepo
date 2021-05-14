a6c2cbd23b6716b16d929f3e4e2134a33df93671
an10nsua1 <83958733+an10nsua1@users.noreply.github.com>
2021-05-14 11:42:56 +0200

    Update publicMachine.sh

--- publicMachine.sh
+++ publicMachine.sh
@@ -5,17 +5,12 @@ apt install openjdk-8-jdk
 Xvfb :1 -ac -screen 0 1024x768x24 &
 export DISPLAY=:1 ; x11vnc -ncache 10 -ncache_cr -display :1 -forever -shared -logappend /var/log/x11vnc.log -bg -noipv6
 echo "VNC initialized"
-
 cd /root
-
 echo "Initiating IBGateway..."
-
 wget https://download2.interactivebrokers.com/installers/ibgateway/latest-standalone/ibgateway-latest-standalone-linux-x64.sh
 chmod u+x ibgateway-latest-standalone-linux-x64.sh
 sh ibgateway-latest-standalone-linux-x64.sh -c -y
-
 echo "IBGateway started"
-
 wget https://github.com/ib-controller/ib-controller/releases/download/3.4.0/IBController-3.4.0.zip
 unzip ./IBController-3.4.0.zip -d ./ibcontroller.paper
 #Sustitucion de IBGatewayStart.sh
