b4ba66523d8dc090a4c717ba8be4b052fc0e9dbd
an10nsua1 <83958733+an10nsua1@users.noreply.github.com>
2021-05-14 11:34:00 +0200

    Update publicMachine.sh

--- publicMachine.sh
+++ publicMachine.sh
@@ -2,7 +2,6 @@
 echo "Downloading VNC"
 apt install vim wget curl net-tools unzip xvfb x11vnc
 apt install openjdk-8-jdk
-
 Xvfb :1 -ac -screen 0 1024x768x24 &
 export DISPLAY=:1
 x11vnc -ncache 10 -ncache_cr -display :1 -forever -shared -logappend /var/log/x11vnc.log -bg -noipv6
