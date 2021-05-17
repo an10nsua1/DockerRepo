9ff7fffe37e5256360ac739564d5bcc9875cad46
an10nsua1 <83958733+an10nsua1@users.noreply.github.com>
2021-05-17 11:32:36 +0200

    Update publicMachine.sh

--- publicMachine.sh
+++ publicMachine.sh
@@ -8,15 +8,15 @@ cd /root
 echo "Initiating IBGateway..."
 wget https://download2.interactivebrokers.com/installers/ibgateway/latest-standalone/ibgateway-latest-standalone-linux-x64.sh
 chmod u+x ibgateway-latest-standalone-linux-x64.sh
-"/root/Jts/ibgateway/984" yes| sh ibgateway-latest-standalone-linux-x64.sh -c
+yes /root/Jts/ibgateway/984| sh ibgateway-latest-standalone-linux-x64.sh -c
 echo "IBGateway started"
 wget https://github.com/ib-controller/ib-controller/releases/download/3.4.0/IBController-3.4.0.zip
 apt-get install unzip
 unzip ./IBController-3.4.0.zip -d ./ibcontroller.paper
 #Sustitucion de IBGatewayStart.sh
 wget https://raw.githubusercontent.com/an10nsua1/DockerRepo/main/IBControllerGatewayStart.sh
-rm /root/ibcontroller.paper/IBControllerGatewayStart.sh
-cp IBControllerGatewayStart.sh /root/ibcontroller.paper
+rm ./ibcontroller.paper/IBControllerGatewayStart.sh
+cp IBControllerGatewayStart.sh ./ibcontroller.paper
 rm IBControllerGatewayStart.sh
 chmod u+x ./ibcontroller.paper/*.sh ./ibcontroller.paper/*/*.sh
 
@@ -26,15 +26,15 @@ pwd
 
 #Sustitucion de IBController.ini
 wget https://raw.githubusercontent.com/an10nsua1/DockerRepo/main/IBController.ini
-rm /root/ibcontroller.paper/IBController.ini
-cp IBController.ini /root/ibcontroller.paper/
+rm ./ibcontroller.paper/IBController.ini
+cp IBController.ini ./ibcontroller.paper/
 rm IBController.ini
 
 #Sustitucion de Jts.ini
 
 wget https://raw.githubusercontent.com/an10nsua1/DockerRepo/main/jts.ini
-rm /root/Jts/jts.ini
-cp jts.ini /root/Jts/
+rm ./Jts/jts.ini
+cp jts.ini ./Jts/
 rm jts.ini
 
 DISPLAY=:1 ~/ibcontroller.paper/IBControllerGatewayStart.sh
